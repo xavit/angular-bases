62ef3faf05b9454b53e7a6d2ee15f2faf9139fb7
Javier Claros <javiclaros@gmail.com>
2023-11-07 17:04:43 -0400

    agregue hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent],
+  declarations: [AppComponent, CounterComponent, HeroComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
