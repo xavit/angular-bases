164c877e3d7775b8cfb2f9ec9bcb76a7b9954a71
Javier Claros <javiclaros@gmail.com>
2023-11-07 18:58:18 -0400

    componentes hero y hero list

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css'],
 })
-export class HeroComponent {}
+export class HeroComponent {
+  public name: string = 'Ironman';
+
+  public age: number = 45;
+
+  public get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+}
